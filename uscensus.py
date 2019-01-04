f3a730cea5d660bdd84abf58f6bd4fda1c7abb52
Nitinraj <nairnitinraj95@gmail.com>
2019-01-04 02:54:17 -0500

    second commit

--- uscensus.py
+++ uscensus.py
@@ -37,9 +37,44 @@ def main():
     #
     # df.groupby(["Job-Type"]).size().plot(kind="bar", fontsize=14)
     # plt.show()
-    print(df["fnlwt"].value_counts())
-    # df.groupby(["fnlwt"]).size().plot(kind="bar", fontsize=14)
+    # df = df.drop(columns='fnlwgt')
+    # df["Education-level"] = df["Education-level"].map({'12th':7, '10th':5, '7th-8th':3, 'Assoc-acdm':11, 'Doctorate':16, 'Assoc-voc':12, '11th':6, 'Bachelors':14, '9th':4, 'Prof-school':9,
+    #  'Some-college':10, '1st-4th':1, 'HS-grad':13, 'Preschool':8, 'Masters':15, '5th-6th':2})
+    #
+    # df.groupby(["Education-level"]).size().plot(kind="bar", fontsize=14)
     # plt.show()
+
+    # df['Marital Status'] = df['Marital Status'].map({'Married-spouse-absent': 1, 'Widowed': 1,'Married-civ-spouse': 2, 'Separated': 3, 'Divorced': 4,
+    #                                                      'Never-married': 5, 'Married-AF-spouse': 6})
+    #
+    # df['Sex'] = df['Sex'].map({'Male': 0, 'Female': 1}).astype(int)
+    #
+    # df['Race'] = df['Race'].map({'Black': 0, 'Asian-Pac-Islander': 1, 'Other': 2, 'White': 3,
+    #                                  'Amer-Indian-Eskimo': 4})
+    #
+    # df['Status'] = df['Status'].map({'Unmarried': 0, 'Other-relative': 1, 'Not-in-family': 2,
+    #                                              'Wife': 3, 'Husband': 4, 'Own-child': 5})
+    df['Country'].replace("?", np.nan, inplace=True)
+    df['Country']=df['Country'].dropna()
+    df['Country'] = df['Country'].map({'Puerto-Rico': 0, 'Haiti': 1, 'Cuba': 2, 'Iran': 3,
+                                                     'Honduras': 4, 'Jamaica': 5, 'Vietnam': 6, 'Mexico': 7,
+                                                     'Dominican-Republic': 8,
+                                                     'Laos': 9, 'Ecuador': 10, 'El-Salvador': 11, 'Cambodia': 12,
+                                                     'Columbia': 13,
+                                                     'Guatemala': 14, 'South': 15, 'India': 16, 'Nicaragua': 17,
+                                                     'Yugoslavia': 18,
+                                                     'Philippines': 19, 'Thailand': 20, 'Trinadad&Tobago': 21,
+                                                     'Peru': 22, 'Poland': 23,
+                                                     'China': 24, 'Hungary': 25, 'Greece': 26, 'Taiwan': 27,
+                                                     'Italy': 28, 'Portugal': 29,
+                                                     'France': 30, 'Hong': 31, 'England': 32, 'Scotland': 33,
+                                                     'Ireland': 34,
+                                                     'Holand-Netherlands': 35, 'Canada': 36, 'Germany': 37, 'Japan': 38,
+                                                     'Outlying-US(Guam-USVI-etc)': 39, 'United-States': 40
+                                                     })
+    df.groupby(["Country"]).size().plot(kind="bar", fontsize=14)
+    plt.show()
+
 
 
 
